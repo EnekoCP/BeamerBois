a0e24f58409a86f74f4766f0619eb4c8d868c82a
VitorAzevedo76 <joao.76.azevedo@gmail.com>
2019-04-27 16:00:25 +0200

    las nuevas excepciones

---
+++ proyecto2/src/PROYECTO/ProtaHaGanadoExcepcion.java
@@ -0,0 +1,16 @@
+package PROYECTO;
+
+public class ProtaHaGanadoExcepcion extends Exception{
+	//atributos 
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//constructura
+	public ProtaHaGanadoExcepcion(){
+		super();
+	}
+	//otros_metodos
+}
---
+++ proyecto2/src/PROYECTO/ProtaHaMuertoExcepcion.java
@@ -0,0 +1,16 @@
+package PROYECTO;
+
+public class ProtaHaMuertoExcepcion extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//atributos 
+
+	//constructura
+public ProtaHaMuertoExcepcion(){
+	super();
+}
+	//otros_metodos
+}
